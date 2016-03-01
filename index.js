c1311a7ec157c847ae86d6af32b36db3ed8767cd
swibge <matthieu.pinte@gmail.com>
2016-03-01 11:17:45 +0100

    Initial commit... -> create a location link to maps

---
+++ index.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import Location from './Location'
+import mjml from '../mjml/lib'
+
+/*
+  Compile an mjml string
+*/
+const htmlOutput = mjml.mjml2html(`<mj-body>
+  <mj-section>
+    <mj-column width="50%">
+      <mj-location address="37bis, Rue du Sentier, 75002 Paris, France" text="Chez moi" />
+    </mj-column>
+  </mj-section>
+</mj-body>
+`)
+
+/*
+  Print the responsive HTML generated
+*/
+fs.writeFile('output.html', htmlOutput);
